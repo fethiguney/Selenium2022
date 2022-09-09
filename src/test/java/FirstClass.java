9c9d93b069aca1e391c3b1c6f6896c208993e768
Fethi <fethiguneyy@gmail.com>
2022-09-09 22:01:51 +0300

    "09/09/2022"

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -4,9 +4,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FirstClass {
     public static void main(String[] args) {
 
+         /*
+        En temel haliyle bir otomasyon yapmak için
+        Class'ımıza otomasyon için gerekli olan webdriver'in yerini göstermemiz gerekir
+        bunun için java kutuphanesinde System.setProperty() method'unu kullanırız
+        ve metthod'un içine ilk olarak driver'i yazarız. İkinci olarak onun fiziki yolunu kopyalarız
+         */
+
         System.setProperty("webdriver.chrome.driver", "src/resources/drivers/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
-        driver.get("https://www.techproeducation.com");
+        driver.get("https://istanbuldagez.net");
+
+
+
+
 
     }
 }
